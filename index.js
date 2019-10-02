e22816eeb1758ed4fdfe20a97a9c9d94c3b336ca
Aditya Pathak <46691394+adityasunny1189@users.noreply.github.com>
2019-10-02 10:02:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ document.querySelector("button").addEventListener("click" , function () {
   var t2sc1 = document.querySelector(".t2rd1").value;
   var t2sc2 = document.querySelector(".t2rd2").value;
   var t2sc3 = document.querySelector(".t2rd3").value;
-  var t1avgscr = (t1sc1 + t1sc2 + t1sc3) / 3;
-  var t2avgscr = (t2sc1 + t2sc2 + t2sc3) / 3;
-  if(t1avgscr > t2avgscr)
+  var t1_total_scr = t1sc1 + t1sc2 + t1sc3;
+  var t2_total_scr = t2sc1 + t2sc2 + t2sc3;
+  var t1avgscr = t1_total_scr/3;
+  var t2avgscr = t2_total_scr/3;
+  console.log(t1avgscr);
+  console.log(t2avgscr);
+  console.log(t1_total_scr);
+  console.log(t2_total_scr);
+  console.log(t1sc1);
+  console.log(t1sc2);
+  console.log(t1sc3);
+  console.log(t2sc1);
+  console.log(t2sc2);
+  console.log(t2sc3);
+  if(t1_total_scr > t2_total_scr)
   {
     document.querySelector(".content").innerHTML = "Team 1 is winner";
     document.querySelector('.first').style.display = 'flex';
@@ -16,7 +28,7 @@ document.querySelector("button").addEventListener("click" , function () {
         document.querySelector('.first').style.display = 'none';
       });
   }
-  else if(t1avgscr === t2avgscr)
+  else if(t1_total_scr === t2_total_scr)
   {
     document.querySelector(".content").innerHTML = "Match Tie";
     document.querySelector('.first').style.display = 'flex';
